c97882766ac944a6715beabe0cc94c4f14f57be3
Erin Stalzer <erinstalzer@gmail.com>
2019-05-28 18:04:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -112,7 +112,8 @@ def self.find_by_name(name)
   end
 
   def update
-    if self.id ==nil
+    if self.id == nil
+      self.save
     else
      sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
      DB[:conn].execute(sql, self.name, self.breed, self.id)
