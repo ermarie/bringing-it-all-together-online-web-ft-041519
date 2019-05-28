be24029b8d11b87c1c3885940c5dbe89998a3bd7
Erin Stalzer <erinstalzer@gmail.com>
2019-05-28 18:14:26 +0000

    Automatically backed up by Learn

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -140,10 +140,13 @@ describe "Dog" do
 
   describe '#update' do
     it 'updates the record associated with a given instance' do
+      binding.pry
       teddy.save
       teddy.name = "Teddy Jr."
+      binding.pry
       teddy.update
       teddy_jr = Dog.find_by_name("Teddy Jr.")
+      binding.pry
       expect(teddy_jr.id).to eq(teddy.id)
     end
 
