5c3f5c686c336b381c1f0388dd6f738af528361a
Erin Stalzer <erinstalzer@gmail.com>
2019-05-28 17:48:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,7 +48,7 @@ class Dog
      dog = Dog.new(hash)
      dog.save
      binding.pry
-     if !dog.id == nil
+     if dog.id == nil
        binding.pry
        dog.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
      end
