853b39131f35267c5035c8f7839146957cf300dc
Erin Stalzer <erinstalzer@gmail.com>
2019-05-28 16:32:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 
-  DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+  @@all = []
 
   attr_accessor :id, :name, :breed
 
@@ -8,6 +8,11 @@ class Dog
     @id = hash[:id]
     @name = hash[:name]
     @breed = hash[:breed]
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def self.create_table
@@ -21,6 +26,93 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    self
+  end
+
+  def self.create(hash)
+    self.create_table
+     dog = Dog.new(hash)
+     dog.save
+     if !dog.id == nil
+       dog.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+     end
+     dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE dogs.id = ?
+    SQL
+
+    hash = {
+      :id => id
+    }
+
+    DB[:conn].execute(sql, id).map do |row|
+      hash[:name] = row[1]
+      hash[:breed] = row[2]
+    end
+    found_dog = Dog.all.find {|dog| dog.id == id}
+    found_dog
+  end
+
+  def self.find_or_create_by(name:, breed:)
+
+    self.find_by_name(name)
+
+    hash = {
+        :name => name,
+        :breed => breed
+      }
+    new_dog = self.create(hash)
+    new_dog
+
+    dogs = self.find_by_name(name)
+
+new_dog
+
+
+  end
+
+  def self.new_from_db(row)
+  name =  row[1]
+  breed = row[2]
+  dog = self.new(name, breed)  # self.new is the same as running student.new
+  dog.id = row[0]
+  dog  # return the newly created instance
+end
+
+def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+     DB[:conn].execute(sql, self.name, self.breed, self.id)
+   end
 
 end
